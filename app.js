13d279249887cec72fc236d05dff5e03a54b5940
Naman Bhatia <bhatia.naman@hotmail.com>
2020-09-14 17:30:02 +0530

    data retrieved

--- app.js
+++ app.js
@@ -3,17 +3,13 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
+const ejs = require('ejs');
 
 const app = express();
 app.use(express.static("Public"));
 app.use(bodyParser.urlencoded({extended: true}));
+app.set('view engine', 'ejs');
 
-
-app.listen(3000, function(){
-
-console.log("Server is running on PORT 3000");
-
-})
 
 
 app.get("/", function(req,res){
@@ -26,24 +22,51 @@ res.sendFile(__dirname + "/home.html")
 app.post("/", function(req,res){
  
     var ipAddress = req.body.ip;
-    var URL = "https://ipvigilante.com/json/" + ipAddress
+    var URL = "http://ip-api.com/json/" + ipAddress
 
     
 
 
     request(URL, function(error,response, body){
-
-        
-
         var loc = JSON.parse(body)
-        var country = loc.data.country_name;
+        var country = loc.country;
+        var countryCode = loc.countryCode;
+        var region = loc.regionName;
+        var city = loc.city;
+        var zip = loc.zip;
+        var latitude = loc.lat;
+        var longitude = loc.lon;
+        var timezone = loc.timezone;
+        var isp = loc.isp;
+        var org = loc.org;
+        var asname = loc.as;
+
         
         
         console.log(country);   
 
-        res.send("<h2>Your country is<h2>" + country);
+        res.render('data' ,{
+            country: country,
+            countryCode: countryCode,
+            regionName: region,
+            cityName: city,
+            zipCode: zip,
+            lat: latitude,
+            lon: longitude,
+            timezone: timezone,
+            isp: isp,
+            organization: org,
+            as: asname
+
+        } );
         
     });
    
+    
+})
+
+app.listen(3000, function(){
+
+    console.log("Server is running on PORT 3000");
     
 })
