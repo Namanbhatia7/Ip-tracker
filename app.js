0b3c0dca9113b905275bbd4972857a48a11225cd
Naman Bhatia <bhatia.naman@hotmail.com>
2020-11-26 18:20:51 +0530

    Port changed

--- app.js
+++ app.js
@@ -58,6 +58,6 @@ app.post("/result", function (req, res) {
     });
 })
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log("Server is running on PORT 3000");
 })
